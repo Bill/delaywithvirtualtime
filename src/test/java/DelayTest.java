54aae0f7e7f067eaa3d76526826ea890e1afff72
Bill Burcham <bburcham@pivotal.io>
2019-09-18 10:17:17 -0700

    fixed test

--- src/test/java/DelayTest.java
+++ src/test/java/DelayTest.java
@@ -28,11 +28,11 @@ public class DelayTest {
         .withVirtualTime(() -> Flux.just(1, 2).delayElements(Duration.ofSeconds(2)))
         .expectSubscription()
         .then(() -> System.out.println("got subscription"))
-        .expectNoEvent(Duration.ofSeconds(1))
+        .expectNoEvent(Duration.ofSeconds(2))
         .then(() -> System.out.println("got event"))
         .expectNext(1)
         .then(() -> System.out.println("got 1"))
-        .expectNoEvent(Duration.ofSeconds(1))
+        .expectNoEvent(Duration.ofSeconds(2))
         .expectNext(2)
         .then(() -> System.out.println("got 2"))
         .verifyComplete();
